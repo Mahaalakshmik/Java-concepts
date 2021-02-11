7ca47316d334f61a49d6b9fc85db4c75d5e485a5
Mahaalakshmik <haarikha06@gmail.com>
2021-02-11 13:54:17 +0530

    checking

---
+++ src/com/java/concepts/String_Concepts.java
@@ -0,0 +1,22 @@
+package com.java.concepts;
+
+public class String_Concepts {
+
+	public static void main(String[] args) {
+		String s = "I believe in god";
+		String s1 = "Welcome to the world of JAVA";
+		
+		int length = s.length();
+		System.out.println(length);
+		char result = s.charAt(6);
+		System.out.println(result);
+		if (s==s1) {
+			System.out.println("equal");
+		} else {
+System.out.println("Not equal");
+		}		
+				
+
+	}
+
+}
---
+++ src/com/java/programs/Loops.java
@@ -0,0 +1,11 @@
+package com.java.programs;
+
+public class Loops {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+			
+		}	}
+	
+
+}
